ff5a8ee7cfaebcfd3b154e638fd314e2d415e347
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-22 16:00:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+def my_hash(hash)
+ hash = {"" 
 
-  
 end
 
 
@@ -28,15 +27,11 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
+                  "oil paintings" => 3,
+                  "muskets" => 2,
+                  "gun powder" => 4,
+                                   }
+shipping_manifest
 
   # return the shipping_manifest hash below
 
